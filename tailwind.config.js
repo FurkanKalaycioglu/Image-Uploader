aa1f835eb5ca75532f4371d54ad29928f91f81bd
Furkan Kalaycıoğlu <Furkan0Kalaycioglu@gmail.com>
2022-07-13 00:41:11 +0300

    tailwind ui

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        somesortgray: "#F6F8FB",
+        backgroundwhite: "#FAFAFB",
+        headingcolor: "#4F4F4F",
+      },
+    },
+  },
+  plugins: [],
+};
